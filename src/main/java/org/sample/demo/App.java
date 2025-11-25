43cb8c985101ddeeeb789dba2c2bb6631bd26bf6
PKarthikyadav <karthikandb@gmail.com>
2025-11-25 23:42:36 +0530

    Update output message in App.java

--- src/main/java/org/sample/demo/App.java
+++ src/main/java/org/sample/demo/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("old country in the ...");
+        System.out.println("old country in the movie...");
 
     }
 }
